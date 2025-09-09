d0aed0f1d1d4e0cacbeaa517c0f3bbb68f33381e
wakabss <153412373+wakabss@users.noreply.github.com>
2025-09-09 03:01:38 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function AboutPage() {
           </p>
 
           <p>
-        My reporting focuses on power and the voices that slip beneath it, seeking stories that are both investigative and deeply human. In addition to reporting, I write essays, fiction, and the occasional love letter to strange places. My creative work appears in <em>Panorama: The Journal of Travel, Place, and Nature</em>. 
+        My reporting focuses on power and the voices that slip beneath it, seeking stories that are both investigative and deeply human. In addition to reporting, I write essays, fiction, and the occasional 19th-century-philosopher-esque memoir, usually born in the fever-state of heartbreak. My creative work appears in <em>Panorama: The Journal of Travel, Place, and Nature</em>. 
           </p>
   
           <p>
