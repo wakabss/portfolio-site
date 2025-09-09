c7b21bb23777105faeaab67176af4d8d41bc065e
wakabss <153412373+wakabss@users.noreply.github.com>
2025-09-09 00:59:23 -0400

    Update page.tsx

--- app/creative/page.tsx
+++ app/creative/page.tsx
@@ -2,16 +2,21 @@ export default function CreativePage() {
   return (
     <>
       <h1>Creative Work</h1>
-      <p>Here’s a selection of my essays, memoirs, and fiction:</p>
-      <ul>
-        <li>
-          <a href="https://panoramajournal.org/issues/issue-15-paris/paris-egg-salad-and-other-nudities/?srsltid=AfmBOooD5BWyRGEhiGzlHFKSm9tr2JBAZiTqVMmdFz1F_jdBGUswWbLY" target="_blank">
-            “Egg Salad and Other Nudities” – Panorama: Journal of Travel, Place, and Nature
-          </a>
-        </li>
-      
-      </ul>
+
+      <div style={{ fontSize: '0.9rem', lineHeight: 1.6 }}>
+        <p>Here’s a selection of my essays, memoirs, and fiction:</p>
+        <ul>
+          <li>
+            <a
+              href="https://panoramajournal.org/issues/issue-15-paris/paris-egg-salad-and-other-nudities/?srsltid=AfmBOooD5BWyRGEhiGzlHFKSm9tr2JBAZiTqVMmdFz1F_jdBGUswWbLY"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              “Egg Salad and Other Nudities” – Panorama: Journal of Travel, Place, and Nature
+            </a>
+          </li>
+        </ul>
+      </div>
     </>
   );
 }
-
