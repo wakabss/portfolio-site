1bd991e63faf5eb207006c7c7b89a7327d6d768a
wakabss <153412373+wakabss@users.noreply.github.com>
2025-09-08 23:52:14 -0400

    Add files via upload

---
+++ app/contact/page.tsx
@@ -0,0 +1,15 @@
+export default function ContactPage() {
+  return (
+    <>
+      <h1>Contact</h1>
+      <p>You can reach me for editorial, freelance, or collaboration inquiries at:</p>
+      <p>
+        <a href="mailto:wakaba.oto@engawa.global">wakaba.oto@engawa.global</a>
+      </p>
+      <p>
+        Instagram: <a href="https://www.instagram.com/wakabss/" target="_blank">@wakabss</a><br />
+        
+      </p>
+    </>
+  );
+}
---
+++ app/creative/page.tsx
@@ -0,0 +1,17 @@
+export default function CreativePage() {
+  return (
+    <>
+      <h1>Creative Work</h1>
+      <p>Here’s a selection of my essays, memoirs, and fiction:</p>
+      <ul>
+        <li>
+          <a href="https://panoramajournal.org/issues/issue-15-paris/paris-egg-salad-and-other-nudities/?srsltid=AfmBOooD5BWyRGEhiGzlHFKSm9tr2JBAZiTqVMmdFz1F_jdBGUswWbLY" target="_blank">
+            “Egg Salad and Other Nudities” – Panorama: Journal of Travel, Place, and Nature
+          </a>
+        </li>
+      
+      </ul>
+    </>
+  );
+}
+
