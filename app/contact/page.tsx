bc33d373ca062cb8c8476b465466236b7c6d75c4
wakabss <153412373+wakabss@users.noreply.github.com>
2025-09-09 00:56:48 -0400

    Update page.tsx

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -2,14 +2,18 @@ export default function ContactPage() {
   return (
     <>
       <h1>Contact</h1>
-      <p>You can reach me for editorial, freelance, or collaboration inquiries at:</p>
-      <p>
-        <a href="mailto:wakaba.oto@engawa.global">wakaba.oto@engawa.global</a>
-      </p>
-      <p>
-        Instagram: <a href="https://www.instagram.com/wakabss/" target="_blank">@wakabss</a><br />
-        
-      </p>
+
+      <div style={{ fontSize: '0.9rem', lineHeight: 1.6 }}>
+        <p>You can reach me for editorial, freelance, or collaboration inquiries at:</p>
+
+        <p>
+          <a href="mailto:wakaba.oto@engawa.global">wakaba.oto@engawa.global</a>
+        </p>
+
+        <p>
+          Instagram: <a href="https://www.instagram.com/wakabss/" target="_blank">@wakabss</a>
+        </p>
+      </div>
     </>
   );
 }
