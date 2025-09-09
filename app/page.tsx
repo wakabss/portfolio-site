562d4d27a720820b82827cf95cc229398bc7a04e
wakabss <153412373+wakabss@users.noreply.github.com>
2025-09-09 00:26:04 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,17 +6,12 @@ export default function AboutPage() {
         I’m a writer and journalist based between Tokyo and New York City, with bylines in <em>Tokyo Weekender</em>, <em>The Japan Times</em>, <em>Matador Network</em>, <em>Ms.</em> magazine, and more.
       </p>
       <p>  
-         Raised across three continents, I’ve flown to Morocco to meet a stranger for a first date, worked undercover as a hostess in Tokyo’s red-light district, and completed a seven-day trek to Machu Picchu without any prior training — all in the name of a good story.
-      </p>
+        My reporting focuses on power and the voices that slip beneath it, seeking stories that are both investigative and deeply human. I’ve flown to Morocco to meet a stranger for a first date, gone undercover as a hostess in Tokyo’s red-light district, and completed a seven-day trek to Machu Picchu without any prior training — all in the name of a good story.
       <p>
-      My reporting focuses on power and the voices that slip beneath it, seeking stories that are both investigative and deeply human. Alongside cultural reporting and features, I write narrative travel pieces that blend reflection with practical insight.
+        In addition to reporting, I write essays, fiction, and the occasional love letter to strange places. My creative work appears in <em>Panorama: The Journal of Travel, Place, and Nature</em>. I also do freelance travel writing when I can, which is just a fancy way of saying I’ll go anywhere for a good story.
       </p>  
       <p>
-      I also write creative fiction and nonfiction, with work in <em>Panorama: The Journal of Travel, Place, and Nature</em>. These pieces often emerge from the strangeness that shapes my own life.
-      
-      </p>
-      <p>
-        I’m always open to new and interesting projects!
+        Always open to collaborations, assignments, or adventures. Please free to reach out!
       </p>
       <p>
         <a href="mailto:wakaba.oto@engawa.global">wakaba.oto@engawa.global</a>
