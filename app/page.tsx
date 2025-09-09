92339bb7c46e6e296ef63eb7c8cc0add00f0e883
wakabss <153412373+wakabss@users.noreply.github.com>
2025-09-09 00:39:15 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,26 +2,32 @@ export default function AboutPage() {
   return (
     <>
       <h1>About</h1>
+      
+        <div style={{ fontSize: '0.9rem', lineHeight: '1.6' }}>
 
-      <p>
+
+          <p>
         I’m a writer and journalist based between Tokyo and New York City, with bylines in <em>Tokyo Weekender</em>, <em>The Japan Times</em>, <em>Matador Network</em>, <em>Ms.</em> magazine, and more.
-      </p>
+          </p>
 
-      <p>
+          <p>
         My reporting focuses on power and the voices that slip beneath it, seeking stories that are both investigative and deeply human. I’ve flown to Morocco to meet a stranger for a first date, gone undercover as a hostess in Tokyo’s red-light district, and completed a seven-day trek to Machu Picchu without any prior training — all in the name of a good story.
-      </p>
-
-      <p>
+          </p>
+  
+          <p>
         In addition to reporting, I write essays, fiction, and the occasional love letter to strange places. My creative work appears in <em>Panorama: The Journal of Travel, Place, and Nature</em>. I also do freelance travel writing when I can, which is just a fancy way of saying I’ll go anywhere for a good story.
-      </p>
+          </p>
 
-      <p>
+          <p>
         Always open to collaborations, assignments, or adventures. Please feel free to reach out!
-      </p>
+          </p>
 
-      <p>
-        <a href="mailto:wakaba.oto@engawa.global">wakaba.oto@engawa.global</a>
-      </p>
+          <p>
+          <a href="mailto:wakaba.oto@engawa.global">wakaba.oto@engawa.global</a>
+          </p>
+
+        </div>
+
 
       <hr style={{ margin: '4rem 0' }} />
     </>
