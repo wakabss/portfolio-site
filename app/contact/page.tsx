a410dc83d34eb6201787192506f045b1f0c6dbe3
wakabss <153412373+wakabss@users.noreply.github.com>
2025-10-03 14:52:59 -0400

    Update page.tsx

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,7 +1,7 @@
 export default function ContactPage() {
   return (
     <>
-      <h1>Contact</h1>
+      <h1>contact</h1>
 
       <div style={{ fontSize: '0.9rem', lineHeight: 1.6 }}>
         <p>You can reach me for editorial, freelance, or collaboration inquiries at:</p>
