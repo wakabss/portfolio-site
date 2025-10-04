962406508ceafc2cf5844f0f05c038593999be8a
wakabss <153412373+wakabss@users.noreply.github.com>
2025-10-04 13:28:20 -0400

    Update page.tsx

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -7,9 +7,8 @@ export default function ContactPage() {
         <p>You can reach me for editorial, freelance, or collaboration inquiries at:</p>
 
         <p>
-          <a href="mailto:wakaba.oto@engawa.global">wakaba.oto@engawa.global</a>
+          Email<a href="mailto:wakaba.oto@engawa.global">wakaba.oto@engawa.global</a>
         </p>
-
         <p>
           Instagram: <a href="https://www.instagram.com/wakabss/" target="_blank">@wakabss</a>
         </p>
